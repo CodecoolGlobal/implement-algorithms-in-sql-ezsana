776e8bdd469d58dab89b651b697f56222ece58a4
Zsuzsanna Erdos <ezsana@gmail.com>
2020-04-02 08:55:03 +0200

    Breadth-first and soundex functions

--- breadth-first-search.sql
+++ breadth-first-search.sql
@@ -1,3 +1,105 @@
 -- SQL statements that are used to define the breadth-first search function
 -- Include all your tried solutions in the SQL file
 -- with commenting below the functions the execution times on the tested dictionaries.
+
+/*
+ See the png file graph-full.png. This graph is directed but treat it as undirected (from-to order of nodes on edges
+ doesn't matter) in your function. Edge notes describes the nature of the friendship: p means pending, a means approved,
+ h means hidden, r means rejected.
+ Create a PL/pgSQL function that can do breadth-first traversal from a given person. The function should return the
+ accumulated number of confirmed friends of the given person.
+ Confirmed friends = approved edges here.
+ */
+
+CREATE OR REPLACE FUNCTION check_pair(id1 int, id2 int) RETURNS boolean AS $$
+    DECLARE
+        sum_of_hits int DEFAULT 0;
+    BEGIN
+        SELECT count(*) INTO sum_of_hits FROM (
+            SELECT * FROM friends_edges WHERE edge_point_1 = id1 AND edge_point_2 = id2
+            UNION ALL
+            SELECT * FROM friends_edges WHERE edge_point_1 = id2 AND edge_point_2 = id1
+            ) AS pairs;
+        RETURN sum_of_hits = 0;
+    END;$$
+    LANGUAGE plpgsql;
+
+CREATE TEMPORARY TABLE friends_nodes (
+    id int PRIMARY KEY,
+    name varchar(255)
+);
+
+CREATE TEMPORARY TABLE friends_edges (
+    edge_point_1 int NOT NULL REFERENCES friends_nodes(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    edge_point_2 int NOT NULL REFERENCES friends_nodes(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (edge_point_1, edge_point_2),
+    CHECK ( check_pair(edge_point_1, edge_point_2) )
+);
+
+CREATE INDEX ep1_index ON friends_edges(edge_point_1);
+CREATE INDEX ep2_index ON friends_edges(edge_point_2);
+
+CREATE OR REPLACE FUNCTION insert_values_to_friends_nodes()
+AS $$
+    DECLARE
+        names varchar(255) := '{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",' ||
+                                  '"twelve", "thirteen", "fourteen", "fifteen"}';
+    BEGIN
+        for i in 1..15 loop
+            INSERT INTO friends_nodes VALUES (i, names[i]);
+        end loop;
+    end;$$
+    LANGUAGE plpgsql;
+
+/*
+ Adjacency list of the graph:
+ 1 -> 2 - 4 - 6 - 10;
+ 2 -> 1 - 8 - 11;
+ 3 -> 7 - 10;
+ 4 -> 1;
+ 5 -> 6;
+ 6 -> 1 - 5 - 8;
+ 7 -> 3;
+ 8 -> 2 - 6 - 9;
+ 9 -> 8 - 11 - 15;
+ 10 -> 1 - 3 - 11;
+ 11 -> 2 - 9 - 10;
+ 12 -> 13 - 14;
+ 13 -> 12 - 14;
+ 14 -> 12 - 13;
+ 15 -> 9;
+ */
+
+CREATE OR REPLACE FUNCTION insert_values_to_friends_edges()
+AS $$
+    DECLARE
+        edge_node_pairs int[][] := '{{1,2}, {1,4}, {1,6}, {1,10}, {2,8}, {2,11}, {3,7}, {3,10}, {5,6}, {6,8},' ||
+                                   ' {8,9}, {9,11}, {9,15}, {10,11}, {12,13}, {12,14}, {13,14}}';
+        node_pairs int[];
+    BEGIN
+        foreach node_pairs slice 1 in ARRAY edge_node_pairs
+        loop
+            INSERT INTO friends_edges VALUES (node_pairs[1], node_pairs[2]);
+        end loop;
+    END;$$
+    LANGUAGE plpgsql;
+
+SELECT insert_values_to_friends_nodes();
+SELECT insert_values_to_friends_edges();
+
+-- Function to find out the connections:
+
+CREATE OR REPLACE FUNCTION get_connections(searched_id int) RETURNS table(vertice int) AS $$
+DECLARE
+    ids record;
+BEGIN
+    for ids in
+        SELECT * FROM friends_edges WHERE edge_point_2 = searched_id OR edge_point_1 = searched_id loop
+        if ids.edge_point_2 = searched_id then
+            SELECT ids.edge_point_1, ids.edge_point_2 into ids.edge_point_2, ids.edge_point_1;
+        end if;
+        vertice := ids.edge_point_2;
+        RETURN next;
+    end loop;
+end;$$
+LANGUAGE plpgsql;
--- soundex.sql
+++ soundex.sql
@@ -1,3 +1,94 @@
 -- SQL statements that are used to define the soundex function
 -- Include all your tried solutions in the SQL file
 -- with commenting below the functions the execution times on the tested dictionaries.
+
+/*
+    1. Retain the first letter of the name and drop all other occurrences of a, e, i, o, u, y, h, w.
+    2. Replace consonants with digits as follows (after the first letter):
+        * b, f, p, v → 1
+        * c, g, j, k, q, s, x, z → 2
+        * d, t → 3
+        * l → 4
+        * m, n → 5
+        * r → 6
+    3. If two or more letters with the same number are adjacent in the original name (before step 1),
+        only retain the first letter; also two letters with the same number separated by 'h' or 'w' are
+        coded as a single number, whereas such letters separated by a vowel are coded twice. This rule also
+        applies to the first letter.
+    4. If you have too few letters in your word that you can't assign three numbers, append with zeros until
+        there are three numbers. If you have four or more numbers, retain only the first three.
+
+ The following algorithm is followed by most SQL languages:
+
+    1. Save the first letter. Map all occurrences of a, e, i, o, u, y, h, w. to zero(0)
+    2. Replace all consonants (include the first letter) with digits as in [2.] above.
+    3. Replace all adjacent same digits with one digit, and then remove all the zero (0) digits
+    4. If the saved letter's digit is the same as the resulting first digit, remove the digit (keep the letter).
+    5. Append 3 zeros if result contains less than 3 digits. Remove all except first letter and 3 digits after it (This step same as [4.] in explanation above).
+ */
+
+CREATE OR REPLACE FUNCTION soundex(name varchar) RETURNS varchar AS $$
+   DECLARE
+        first_letter varchar := left(name, 1);
+        tempString varchar := '';
+        arr varchar[];
+        n varchar;
+        arr2 varchar[];
+        len_tempString integer;
+    BEGIN
+        SELECT into arr string_to_array(name, null);
+        foreach n in array arr loop
+            case n
+                when 'a', 'e', 'i', 'o', 'u', 'y', 'h', 'w' then tempString = tempString || '0';
+                when 'b', 'f', 'p', 'v' then tempString = tempString || '1';
+                when 'c', 'g', 'j', 'k', 'q', 's', 'x', 'z' then tempString = tempString || '2';
+                when 'd', 't' then tempString = tempString || '3';
+                when 'l' then tempString = tempString || '4';
+                when 'm', 'n' then tempString = tempString || '5';
+                when 'r' then  tempString = tempString || '6';
+            end case;
+        end loop;
+        len_tempString := length(tempString);
+        SELECT INTO arr2 string_to_array(tempString, null);
+        for i in 1..len_tempString-1 loop
+            for j in i+1..len_tempString loop
+                if arr2[i] <> '0' AND arr2[i] = arr2[j] then
+                    arr2[j] := '0';
+                elsif arr2[i] <> arr2[j] then
+                    exit;
+                end if;
+            end loop;
+        end loop;
+        SELECT into tempString array_to_string(array_remove(arr2, '0'), '');
+        case
+            when substring(tempString, 2, 1) = '1' AND first_letter IN ('b', 'f', 'p', 'v') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+            when substring(tempString, 2, 1) = '2' AND first_letter IN ('c', 'g', 'j', 'k', 'q', 's', 'x', 'z') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+            when substring(tempString, 2, 1) = '3' AND first_letter IN ('d', 't') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+            when substring(tempString, 2, 1) = '4' AND first_letter IN ('l') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+            when substring(tempString, 2, 1) = '5' AND first_letter IN ('m', 'n') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+            when substring(tempString, 2, 1) = '6' AND first_letter IN ('r') then tempString = substring(tempString, 1, 1) || substring(tempString, 3);
+        end case;
+        if length(tempString) < 3 then
+            tempString = tempString || '0' * (3-length(tempString));
+        elsif length(tempString) > 4 then
+            tempString = substring(tempString, 1, 3);
+        end if;
+        RETURN first_letter || tempString;
+   end;
+    $$
+    LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
