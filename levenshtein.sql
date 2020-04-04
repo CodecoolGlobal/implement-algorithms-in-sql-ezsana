208aaa1818602ca1437e5cdfc5bc98efa68c96c6
Zsuzsanna Erdos <ezsana@gmail.com>
2020-04-04 09:58:19 +0200

    Levenshtein finished

--- levenshtein.sql
+++ levenshtein.sql
@@ -2,8 +2,6 @@
 -- Include all your tried solutions in the SQL file
 -- with commenting below the functions the execution times on the tested dictionaries.
 
-SELECT levenshtein('keke', 'koko');
-
 CREATE OR REPLACE FUNCTION levenshtein(s1 varchar, s2 varchar) RETURNS integer AS $$
     DECLARE
         value1 integer;
@@ -13,6 +11,9 @@ CREATE OR REPLACE FUNCTION levenshtein(s1 varchar, s2 varchar) RETURNS integer A
         letters varchar[] := '{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
         letter text;
         lev_number integer := 0;
+        plusCounter integer := 0;
+        minusCounter integer := 0;
+        s3_arr varchar;
     BEGIN
         CREATE TABLE abc (id text);
 
@@ -35,11 +36,21 @@ CREATE OR REPLACE FUNCTION levenshtein(s1 varchar, s2 varchar) RETURNS integer A
         foreach letter in array letters loop
             EXECUTE format('SELECT %I FROM abc WHERE id = $1;', letter) INTO value1 USING 'one';
             EXECUTE format('SELECT %I FROM abc WHERE id = $1;', letter) INTO value2 USING 'two';
-            lev_number = lev_number + abs(value1-value2);
+            if (value2 - value1) < 0 then
+                minusCounter = minusCounter - (value2 - value1);
+            elsif (value2 - value1) > 0 then
+                plusCounter = plusCounter + (value2 - value1);
+            end if;
         end loop;
+        if (minusCounter = plusCounter) then
+            lev_number = plusCounter;
+        elsif (abs(minusCounter) > abs(plusCounter)) then
+            lev_number = abs(minusCounter);
+        elsif (abs(plusCounter) > abs(minusCounter)) then
+            lev_number = abs(plusCounter);
+        end if;
 
         DROP TABLE abc;
-
         RETURN lev_number;
     END;
     $$
